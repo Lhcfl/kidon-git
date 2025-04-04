291030aa1e0933f8332ea2f0e55c6bf552785926
Lhcfl <lhcfl@outlook.com>
2025-04-04 15:26:30 +0800

    add tests for object serialize

--- src/models/object.rs
+++ src/models/object.rs
@@ -59,6 +59,8 @@ impl Store for Object {
 
 #[cfg(test)]
 mod test {
+    use serde_json::json;
+
     use crate::models::{object::Sha1Able, tree::TreeLine, tree::TreeLineKind};
 
     use super::{Blob, Commit, Object, Tree};
@@ -84,5 +86,66 @@ mod test {
             }],
         };
         assert_eq!(tree.sha1(), Object::from(tree).sha1());
+    }
+
+    #[test]
+    fn object_serialize() {
+        let blob = Blob::from("hello world");
+        let object = Object::from(blob.clone());
+        let serialized = serde_json::to_string(&object).unwrap();
+        assert_eq!(
+            serialized,
+            json!({
+                "Blob": "hello world"
+            })
+            .to_string()
+        );
+
+        let commit = Commit {
+            tree: "tree_hash".into(),
+            parent: Some("parent_hash".into()),
+            timestamp: std::time::SystemTime::UNIX_EPOCH,
+            message: "commit message".to_string(),
+        };
+        let object = Object::from(commit.clone());
+        let serialized = serde_json::to_value(object).unwrap();
+        assert_eq!(
+            serialized,
+            json!({
+                "Commit": {
+                    "tree": "tree_hash",
+                    "parent": "parent_hash",
+                    "timestamp": {
+                        "secs_since_epoch": 0,
+                        "nanos_since_epoch": 0
+                    },
+                    "message": "commit message",
+                }
+            })
+        );
+
+        let tree = Tree {
+            objects: vec![TreeLine {
+                kind: TreeLineKind::File,
+                name: "file.txt".to_string(),
+                sha1: "file_sha1".into(),
+            }],
+        };
+        let object = Object::from(tree.clone());
+        let serialized = serde_json::to_value(object).unwrap();
+        assert_eq!(
+            serialized,
+            json!({
+                "Tree": {
+                    "objects": [
+                        {
+                            "kind": "File",
+                            "name": "file.txt",
+                            "sha1": "file_sha1"
+                        }
+                    ]
+                }
+            })
+        );
     }
 }
