05f3a681e2655de041693919699106b395b91862
Lhcfl <lhcfl@outlook.com>
2025-05-29 19:04:53 +0800

    👷 chore: make github auto build ci

--- make_zip.py
+++ make_zip.py
@@ -1,7 +1,7 @@
 import zipfile
 import os
 
-EXCLUDE_DIRS = { ".kidon-git", ".git", "debug", "generated", "test", "rust-git.zip" }
+EXCLUDE_DIRS = { ".kidon-git", ".git", "target", "test", "rust-git.zip" }
 
 def should_exclude(path: str):
     parts = path.split(os.sep)
@@ -22,11 +22,30 @@ def zipdir(path, ziph, arc_path_prefix=""):
             ziph.write(abs_file_path, arcname)
 
 folder = "."
+arc_path_prefix="rust-git"
 
 try:
   os.remove("rust-git.zip")  # 删除旧的 zip 文件
 except FileNotFoundError:
   pass
 
-with zipfile.ZipFile("rust-git.zip", 'w', zipfile.ZIP_DEFLATED) as zipf:
-    zipdir(folder, zipf, arc_path_prefix="rust-git")  # 保证 zip 根目录就是 rust-git/
+with zipfile.ZipFile("rust-git.zip", 'w', zipfile.ZIP_DEFLATED) as ziph:
+    for root, dirs, files in os.walk(folder):
+      # 过滤掉不想进入的子目录（就地修改 dirs）
+      dirs[:] = [d for d in dirs if not should_exclude(os.path.relpath(os.path.join(root, d), folder))]
+      for file in files:
+          rel_dir = os.path.relpath(root, folder)
+          rel_file = os.path.join(rel_dir, file)
+          if should_exclude(rel_file):
+              continue
+          abs_file_path = os.path.join(root, file)
+          arcname = os.path.join(arc_path_prefix, rel_file.removeprefix(".\\"))
+          print(f"zip: {abs_file_path} -> {arcname}")
+          ziph.write(abs_file_path, arcname)
+    
+    try:
+      ziph.write("target/release/rust-git", "target/release/rust-git")
+      ziph.write("target/debug/rust-git", "target/release/rust-git")
+    except FileNotFoundError:
+      print("No binary found in target/release, you should build it first.")
+      exit(1)
