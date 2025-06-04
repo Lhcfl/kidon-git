cfc16db961fe658d5fa9213f09a39ecdbe91c846
lsamc <2322176165@qq.com>
2025-06-04 20:59:01 +0800

    🐛 Tiny fix.

--- test/mytest.sh
+++ test/mytest.sh
@@ -1,22 +1,37 @@
-mkdir mytest
-cp tests/rust-git mytest/
-cd mytest
+# 当前目录位于 testcases
+# 创建 large_file.bin
+dd if=/dev/zero of=large_file.bin bs=10M count=1
+# 创建一个空目录 ad_test2
+mkdir ad_test2
+# 拷贝 rust-git到ad_test2 目录
+cp rust-git ad_test2/
+# 进入 ad_test2 目录
+cd ad_test2
 
+# 执行 rust-git init
 ./rust-git init
-echo "This is a test file" > test.txt
-./rust-git add test.txt
-./rust-git commit -m "Add test file"
+# 创建 main 分支并切换到 main 分支
+./rust-git checkout -b main
+# 拷贝large_file.bin 到 ad_test2 目录
+cp ../large_file.bin ./
+# 添加 large_file.bin
+./rust-git add large_file.bin
+# 提交 large_file.bin
+commit_hash=$(./rust-git commit -m "Add large file" 2>&1)
+# 检查提交是否成功
+if [ -z "$commit_hash" ]; then
+echo "Commit hash not found"
+exit 1
+fi
+echo "Committed changes: $commit_hash"
 
-./rust-git checkout -b test-branch1
-echo "This is a test file in branch 1" > test.txt
-./rust-git add test.txt
-./rust-git commit -m "Update test file in branch 1"
-
-./rust-git checkout master
-./rust-git checkout -b test-branch2
-
-echo "This is a test file in branch 2" > test.txt
-./rust-git add test.txt
-./rust-git commit -m "Update test file in branch 2"
-
-./rust-git merge test-branch1
+# 检查 .git/objects 目录下是否存在对应的对象文件
+object_dir=".git/objects/${commit_hash:0:2}"
+object_file="$object_dir/${commit_hash:2}"
+if [ -d "$object_dir" ] && [ -f "$object_file" ]; then
+echo "Object file $object_file exists"
+else
+echo "Object file $object_file does not exist"
+exit 1
+fi
+echo "Advanced Test 2 passed: Large file is correctly stored"
